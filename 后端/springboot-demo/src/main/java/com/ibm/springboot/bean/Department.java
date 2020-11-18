d93bc2aa9faecb4e43d7e56ee08064de56fd9808
FangAnger <74387929+FangAnger@users.noreply.github.com>
2020-11-18 16:13:02 +0800

    11-18

--- 后端/springboot-demo/src/main/java/com/ibm/springboot/bean/Department.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.springboot.bean;
-
-public class Department {
-
-	private Integer id;
-	private String departmentName;
-
-	
-}
--- 后端/springboot-demo/src/main/java/com/ibm/springboot/controller/MyBatisController.java
+++ 后端/springboot-demo/src/main/java/com/ibm/springboot/controller/MyBatisController.java
@@ -8,60 +8,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.ibm.springboot.bean.Department;
 import com.ibm.springboot.bean.Employee;
-import com.ibm.springboot.mapper.DepartmentMapper;
+
 import com.ibm.springboot.mapper.EmployeeMapper;
+
+
 
 @RestController
 public class MyBatisController {
 
-//	@Autowired
-//	private DepartmentMapper departmentMapper;
+
 	
 	@Autowired
 	private EmployeeMapper employeeMapper;
 	
-	/**
-	 * 增加部门
-	 * @param department
-	 * @return
-	 */
-//	@PostMapping("/department/insert")
-//    public Department insertDept(@RequestBody Department department){
-//        departmentMapper.insertDept(department);
-//        return department;
-//    }
-//	
-//	/**
-//	 * 修改部门
-//	 * @param department
-//	 */
-//	@PutMapping("/department/update")
-//    public void updateDept(@RequestBody Department department){
-//        departmentMapper.updateDept(department);
-//    }
-//	
-//	/**
-//	 * 根据部门id查询部门
-//	 * @param id
-//	 * @return
-//	 */
-//	@GetMapping("/department/query/{id}")
-//    public Department getDepartment(@PathVariable("id") Integer id){
-//        return departmentMapper.getDeptById(id);
-//    }
-//	
-//	/**
-//	 * 根据部门id删除部门
-//	 * @param id
-//	 */
-//	@DeleteMapping("/department/delete/{id}")
-//    public void deleteDepartment(@PathVariable("id") Integer id){
-//        departmentMapper.deleteDeptById(id);
-//    }
-//	
+
+	
+	
 	/**
 	 * 新增员工
 	 * @param employee
